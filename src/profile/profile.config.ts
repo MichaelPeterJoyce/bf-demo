a9f9e2d8f492779aa775ec9f2928b68aa792afda
Michael Joyce <michael.peter.joyce@gmail.com>
2018-11-09 18:59:08 +0000

    Fixed the build process and deleted some boilerplate home module

--- src/profile/profile.config.ts
+++ src/profile/profile.config.ts
@@ -1,14 +1,11 @@
-class ProfileConfig {
-    constructor(private $stateProvider: any){
-       this.setupRoutes();
-    }
-    setupRoutes(){
-        this.$stateProvider
-            .state('profile', {
-                url: '',
-                template: '<profile-settings></profile-settings>'
-            });
-    }
+function ProfileConfig($stateProvider) {
+    $stateProvider
+        .state('profile', {
+            url: '',
+            template: '<profile-settings></profile-settings>'
+        });
+
 }
+
 ProfileConfig.$inject = ['$stateProvider'];
 export default ProfileConfig;
